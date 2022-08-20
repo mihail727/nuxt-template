2c81838e555e3d2da25ea04a2d442f4ed23e21ec
mihail727 <suppnms@gmail.com>
2022-08-20 19:32:01 +0300

    fix: fixed cli codes

--- bin/cli.js
+++ bin/cli.js
@@ -14,16 +14,15 @@ const runCommand = (command) => {
 
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/mihail727/nuxt-template.git ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const afterInstallClearCommand = 'rm -rf .git';
 
 console.log(`Cloning the repository with name ${repoName}`);
 
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
+const cleared = runCommand(afterInstallClearCommand);
+if (!cleared) process.exit(-1);
 
 console.log('Project ready. Follow the following commands to start');
-console.log(`cd ${repoName} && npm start`);
+console.log(`cd ${repoName} && npm install && npm run dev`);
